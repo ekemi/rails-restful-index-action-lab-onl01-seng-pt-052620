9e9e4603c886c579bd87dcc6bec7e571eea12cc7
edmundo papi <edmundopierre@yahoo.com>
2020-10-05 05:44:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,8 @@ class Student < ApplicationController
    def index
      @students = Student.all
    end
+
+   def to_s
+    self.first_name + " " + self.last_name	    self.first_name + " " + self.last_name
+  end
 end
